3c2fc0cbadf6a901ebb470c2c6f19e13e978d35b
Juan.mendez <juan.mendez@ceiba.com.co>
2020-07-23 14:38:51 -0500

    primer commit projecto base

---
+++ src/main/java/com/ceiba/inventariobodega/InventariobodegaApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.inventariobodega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventariobodegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventariobodegaApplication.class, args);
+		System.out.println("hola soy el gradle project");
+	}
+}
---
+++ src/test/java/com/ceiba/inventariobodega/InventariobodegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.inventariobodega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventariobodegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
