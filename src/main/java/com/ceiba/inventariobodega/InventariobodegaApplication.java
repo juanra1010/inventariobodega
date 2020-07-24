8165c00b2d0011dcd22be97255c567d970c14b00
Juan.mendez <juan.mendez@ceiba.com.co>
2020-07-23 23:34:22 -0500

    properties de sonar y jenkinsfile modificado

--- src/main/java/com/ceiba/inventariobodega/InventariobodegaApplication.java
+++ src/main/java/com/ceiba/inventariobodega/InventariobodegaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventariobodegaApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(InventariobodegaApplication.class, args);
 		System.out.println("hola soy el gradle project");
